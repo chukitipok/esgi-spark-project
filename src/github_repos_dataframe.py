030c23f4dae8d73d648afa9869906380a2608f13
chukitipok <antonin.texier@gmail.com>
2021-12-12 19:33:21 +0100

    Create method to get the 10 most used words.

--- src/github_repos_dataframe.py
+++ src/github_repos_dataframe.py
@@ -1,18 +1,35 @@
 from pyspark.sql import DataFrame, SparkSession
 from pyspark.sql.functions import *
+from pyspark.sql.types import ArrayType, Row, StringType, StructField, StructType
+
 
 class GithubReposDataFrame:
 
     def __init__(self, spark: SparkSession) -> None:
+        self.spark = spark
         self.dataset = spark.read.option('header', 'true').csv('./resources/full.csv')
-        spark.sql("set spark.sql.legacy.timeParserPolicy=LEGACY")
+        self.spark.sql("set spark.sql.legacy.timeParserPolicy=LEGACY")
+        self.stop_words = self.__prepare_stop_words()
 
+    def __prepare_stop_words(self):
+        df = self.spark.read.option('header', 'true').text('./resources/stop_words_en.txt')
+        stop_words = [row.value for row in df.collect()]
+
+        rdd = self.spark.sparkContext.parallelize([Row('tmp', stop_words)])
+
+        schema = StructType([
+            StructField("tmp", StringType(), False),
+            StructField("values", ArrayType(StringType(), False), False)
+        ])
+
+        return self.spark.createDataFrame(rdd, schema)
+    
     def repos_with_most_commits(self, limit: int = 10) -> DataFrame:
         return self.dataset.select('repo')\
             .where('repo is not null')\
             .groupBy('repo')\
             .count()\
-            .orderBy('count', ascending=False)\
+            .orderBy('count', ascending =False)\
             .withColumnRenamed('repo', 'Projects')\
             .withColumnRenamed('count', 'Commits number')\
             .limit(limit)
@@ -22,7 +39,7 @@ class GithubReposDataFrame:
             .where(col('repo') == project_name)\
             .groupBy('author')\
             .count()\
-            .orderBy('count', ascending=False)\
+            .orderBy('count', ascending = False)\
             .first()\
             
         return row.author if row is not None else 'No commits made on "' + project_name + '".'
@@ -33,7 +50,19 @@ class GithubReposDataFrame:
             .where((col('repo') == project_name) & (col('months') <= last_months) & (col('months') >= 0))\
             .groupBy('author')\
             .count()\
-            .orderBy('count', ascending=False)\
+            .orderBy('count', ascending = False)\
             .first()\
             
         return row.author if row is not None else 'No commits made the last ' + str(last_months) + ' months on "' + project_name + '".'
+
+    def words_most_used_in_commits(self, limit: int = 10) -> DataFrame:        
+        return self.dataset.withColumn('tmp', lit('tmp')) \
+            .join(self.stop_words, on = ['tmp']) \
+            .drop('tmp') \
+            .withColumnRenamed('values', 'stop_words') \
+            .select(array_except(split('message', ' '), col('stop_words')).alias('messages')) \
+            .withColumn('word', explode(col('messages'))) \
+            .groupBy('word') \
+            .count() \
+            .sort('count', ascending = False) \
+            .limit(limit)
--- src/main.py
+++ src/main.py
@@ -1,19 +1,28 @@
+import os
 from pyspark.sql import SparkSession
 from github_repos_dataframe import GithubReposDataFrame
 
 if __name__ == '__main__':
-    spark = SparkSession.builder.appName('spark-project').getOrCreate()
-
+    spark = SparkSession.builder.master('local').appName('spark-project').getOrCreate()
+    spark.sparkContext.setLogLevel('OFF')
     github_repo = GithubReposDataFrame(spark)
+    
+    os.system('clear')
 
-    print()
+    # Question 1:
     print('The 10 projects with the most commits are:')
-    github_repo.repos_with_most_commits().show(truncate=False)
+    github_repo.repos_with_most_commits().show(truncate = False)
 
+    # Question 2:
     print('The best contributor on "apache/spark" project is: ' + github_repo.best_contributor('apache/spark'))
     print()
 
+    # Question 3:
     print('The best contributor on "apache/spark" project for the last 6 months is: ' + github_repo.best_contributor_on_last_x_months('apache/spark', 6))
     print()
+
+    # Question 4:
+    print('The 10 words most used in commits message are:')
+    github_repo.words_most_used_in_commits().show(truncate = False)
 
     spark.stop()

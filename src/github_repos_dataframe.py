054ec4d2f5aa813e04f90dd97f22edc36f70e8b3
chukitipok <antonin.texier@gmail.com>
2021-12-12 13:07:09 +0100

    Add method to retrieve best contributor on months.

--- src/github_repos_dataframe.py
+++ src/github_repos_dataframe.py
@@ -1,11 +1,13 @@
-from pyspark.sql.dataframe import DataFrame
+from pyspark.sql import DataFrame, SparkSession
+from pyspark.sql.functions import *
 
 class GithubReposDataFrame:
 
-    def __init__(self, spark) -> None:
+    def __init__(self, spark: SparkSession) -> None:
         self.dataset = spark.read.option('header', 'true').csv('./resources/full.csv')
+        spark.sql("set spark.sql.legacy.timeParserPolicy=LEGACY")
 
-    def repos_with_most_commits(self, limit = 10) -> DataFrame:
+    def repos_with_most_commits(self, limit: int = 10) -> DataFrame:
         return self.dataset.select('repo')\
             .where('repo is not null')\
             .groupBy('repo')\
@@ -15,12 +17,23 @@ class GithubReposDataFrame:
             .withColumnRenamed('count', 'Commits number')\
             .limit(limit)
 
-    def best_contributor(self, project_name) -> str:
+    def best_contributor(self, project_name: str) -> str:
         row = self.dataset.select('author')\
-            .where(self.dataset.repo == project_name)\
+            .where(col('repo') == project_name)\
             .groupBy('author')\
             .count()\
             .orderBy('count', ascending=False)\
             .first()\
             
         return row.author if row is not None else 'No commits made on "' + project_name + '".'
+
+    def best_contributor_on_last_x_months(self, project_name: str, last_months: int) -> str:
+        pattern = "E LLL d HH:mm:ss yyyy Z"
+        row = self.dataset.select('author', 'date', months_between(current_date(), to_date('date', pattern)).alias('months'))\
+            .where((col('repo') == project_name) & (col('months') <= last_months) & (col('months') >= 0))\
+            .groupBy('author')\
+            .count()\
+            .orderBy('count', ascending=False)\
+            .first()\
+            
+        return row.author if row is not None else 'No commits made the last ' + str(last_months) + ' months on "' + project_name + '".'
--- src/main.py
+++ src/main.py
@@ -8,10 +8,12 @@ if __name__ == '__main__':
 
     print()
     print('The 10 projects with the most commits are:')
-    github_repo.repos_with_most_commits().show()
-    print()
+    github_repo.repos_with_most_commits().show(truncate=False)
 
     print('The best contributor on "apache/spark" project is: ' + github_repo.best_contributor('apache/spark'))
+    print()
+
+    print('The best contributor on "apache/spark" project for the last 6 months is: ' + github_repo.best_contributor_on_last_x_months('apache/spark', 6))
     print()
 
     spark.stop()
